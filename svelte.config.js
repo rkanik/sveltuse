e0785ca6717e87b8417e3085864ebe23c3f2f85c
RK Anik <79830581+encoderitanik@users.noreply.github.com>
2023-05-19 22:25:48 +0600

    Added cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import adapter from '@sveltejs/adapter-vercel'
+import cloudflare from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
@@ -21,7 +18,12 @@ const config = {
 		}
 	},
 	kit: {
-		adapter: adapter({ runtime: 'edge' }),
+		adapter: cloudflare({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
 		alias: {
 			'@shared': 'src/lib/shared',
 			'@shared/*': 'src/lib/shared/*',
